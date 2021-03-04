87852905315c7bbda43e8cbfcb617e8acde70b5f
Ravindra Purohit <ravindrapurohit2011@gmail.com>
2021-03-04 13:32:08 +0530

    'stores-data-in-local-storage'

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const filterOption = document.querySelector('.filter-todo')
 
 
 // Event Listeners
+document.addEventListener('DOMContentLoaded', getTodos);
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
 filterOption.addEventListener('click', filterTodo);
@@ -22,6 +23,10 @@ function addTodo(event) {
     newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
+
+    // Add to Local Storage
+    saveLocalTodos(todoInput.value);
+
     // Check mark button
     const completedButton = document.createElement('button');
     completedButton.innerHTML = '<i class="fas fa-check"></i>';
@@ -84,3 +89,53 @@ function filterTodo(e) {
         }
     });
 }                                                                                                                                               
+
+
+function saveLocalTodos(todo) {
+    // Check
+    let todos;
+    if (localStorage.getItem('todos') === null){
+        todos = [];
+    }
+    else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+function getTodos(todo) {
+    // Check
+    let todos;
+    if (localStorage.getItem('todos') === null){
+        todos = [];
+    }
+    else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+
+    todos.forEach(function(todo){
+        // Todo Div
+        const todoDiv = document.createElement('div');
+        todoDiv.classList.add('todo');
+        // create Li
+        const newTodo = document.createElement('li');
+        newTodo.innerText = todo;
+        newTodo.classList.add('todo-item');
+        todoDiv.appendChild(newTodo);
+
+        // Check mark button
+        const completedButton = document.createElement('button');
+        completedButton.innerHTML = '<i class="fas fa-check"></i>';
+        completedButton.classList.add('complete-btn');
+        todoDiv.appendChild(completedButton);
+        // Check trash button
+        const trashButton = document.createElement('button');
+        trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+        trashButton.classList.add('trash-btn')
+        todoDiv.appendChild(trashButton);
+        // Append To UL List
+        todoList.appendChild(todoDiv);
+    })
+}
